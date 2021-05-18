9ca2ee9285130139214ffe2b2d4e6df4a479bd06
danderle <danderle86@gmail.com>
2021-05-18 08:47:56 +0200

    Added the generic BaseAttachedProperties to create Attached Properties. Also added the GridAttachedProperties.cs class which has the ShuffleGridProperty implemented

---
+++ GenAlpha/AttachedProperties/BaseAttachedProperties.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows;
+
+namespace GenAlpha
+{
+    /// <summary>
+    /// A base attached property to replace the vanilla WPF attached property
+    /// </summary>
+    /// <typeparam name="Parent">The parent class to be attached property</typeparam>
+    /// <typeparam name="Property">The type of this attached property</typeparam>
+    public abstract class BaseAttachedProperties<Parent, Property>
+        where Parent : BaseAttachedProperties<Parent, Property>, new()
+    {
+        #region Public Events
+
+        /// <summary>
+        /// Fired when the value changes
+        /// </summary>
+        public event Action<DependencyObject, DependencyPropertyChangedEventArgs> ValueChanged = (sender, e) => { };
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// A singleton instance of our parent class
+        /// </summary>
+        public static Parent Instance { get; private set; } = new Parent();
+
+        #endregion
+
+        #region Attached Property Definitions
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.RegisterAttached("Value", typeof(Property), typeof(BaseAttachedProperties<Parent, Property>), new PropertyMetadata(new PropertyChangedCallback(OnValuePropertyChanged)));
+
+        /// <summary>
+        /// The callback event when the <see cref="ValueProperty"/> is changed
+        /// </summary>
+        /// <param name="d">The UI element that has its property changed</param>
+        /// <param name="e">The arguments for the event</param>
+        private static void OnValuePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            //Call the parent function
+            Instance.OnValueChanged(d, e);
+
+            //call event listeners
+            Instance.ValueChanged(d, e);
+        }
+
+        /// <summary>
+        /// Gets the attached property
+        /// </summary>
+        /// <param name="d">The element to get the property from</param>
+        /// <returns></returns>
+        public static Property GetValue(DependencyObject d) => (Property)d.GetValue(ValueProperty);
+
+        /// <summary>
+        /// Sets the attached property
+        /// </summary>
+        /// <param name="d">The element to get the property from</param>
+        /// <param name="value">The value to set the property to</param>
+        public static void SetValue(DependencyObject d, Property value) => d.SetValue(ValueProperty, value);
+
+        #endregion
+
+        #region Event Methods
+
+        /// <summary>
+        /// The method that is called when any attached property of this type is changed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public virtual void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e) { }
+
+        #endregion
+    }
+}
---
+++ GenAlpha/AttachedProperties/GridAttachedProperties.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GenAlpha
+{
+    /// <summary>
+    /// Shuffles all the items in the grid to new positions
+    /// </summary>
+    public class ShuffleGridProperty : BaseAttachedProperties<ShuffleGridProperty, bool>
+    {
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            //Get the caller
+            var grid = sender as Grid;
+
+            //Make sure it is a grid
+            if (grid == null)
+                return;
+
+            if(!grid.IsLoaded)
+            {
+                grid.Loaded += Grid_Loaded;
+                return;
+            }
+
+            //true to shuffle
+            if((bool)e.NewValue)
+            {
+                var list = new List<Button>();
+                int count = 0;
+                foreach(var item in grid.Children)
+                {
+                    var button = item as Button;
+                    button.Tag = count;
+                    list.Add(button);
+                    count++;
+                }
+                grid.Children.Clear();
+                foreach (var button in list)
+                {
+                    grid.Children.Add(button);
+                }
+            }
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            var grid = sender as Grid;
+            var list = new List<Button>();
+            int count = 0;
+            var cards = (ResourceDictionary)Application.Current.Resources["MemoryCards"];
+            if(cards.Count * 2 == grid.Children.Count)
+            {
+                int gridIndex = 0;
+                foreach(DictionaryEntry card in cards)
+                {
+                    var key = card.Key;
+                    var value = card.Key;
+                    var button = (Button)grid.Children[gridIndex];
+                    button.Tag = key.ToString().Replace("FontAwesome", "");
+                    button.Content = Application.Current.Resources[key];
+                    list.Add(button);
+                    gridIndex++;
+                    button = (Button)grid.Children[gridIndex];
+                    button.Tag = key.ToString().Replace("FontAwesome", "");
+                    button.Content = Application.Current.Resources[key];
+                    list.Add(button);
+                    gridIndex++;
+                }
+            }
+
+            Shuffle(list);
+
+            
+            grid.Children.Clear();
+            int row = 0;
+            int column = 0;
+            foreach (var button in list)
+            {
+                Grid.SetColumn(button, column);
+                Grid.SetRow(button, row);
+                column++;
+                if(column >= grid.ColumnDefinitions.Count)
+                {
+                    column = 0;
+                    row++;
+                }
+                grid.Children.Add(button);
+            }
+        }
+
+        private void Shuffle<T>(IList<T> list)
+        {
+            Random random = new Random();
+            int n = list.Count;
+
+            for (int i = list.Count - 1; i > 1; i--)
+            {
+                int rnd = random.Next(i + 1);
+
+                T value = list[rnd];
+                list[rnd] = list[i];
+                list[i] = value;
+            }
+        }
+    }
+}
