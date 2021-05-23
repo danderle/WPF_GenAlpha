8014ba490501194f64644ba6b18417b46bbd5f8a
danderle <danderle86@gmail.com>
2021-05-23 22:05:32 +0200

    Added GameSelection and Memory view model

---
+++ GenAlpha/ViewModels/GameSelectionViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace GenAlpha
+{
+    /// <summary>
+    /// The view model for the <see cref="GameSelectionPage"/>
+    /// </summary>
+    public class GameSelectionViewModel : BaseViewModel
+    {
+        #region Private Members
+
+        
+
+        #endregion
+
+        #region Properties
+
+        
+
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// The command to minimize the window
+        /// </summary>
+        public ICommand MinimizeCommand { get; set; }
+
+        /// <summary>
+        /// The command to maximize the window
+        /// </summary>
+        public ICommand MaximizeCommand { get; set; }
+
+        /// <summary>
+        /// The command to close the window
+        /// </summary>
+        public ICommand CloseCommand { get; set; }
+
+        /// <summary>
+        /// The command to open the system menu the window
+        /// </summary>
+        public ICommand MenuCommand { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public GameSelectionViewModel()
+        {
+            
+        }
+
+        #endregion
+
+       
+        #region Private Helpers
+
+        #endregion
+    }
+}
---
+++ GenAlpha/ViewModels/MemoryViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace GenAlpha
+{
+    /// <summary>
+    /// The view model for the <see cref="MemoryPage"/>
+    /// </summary>
+    public class MemoryViewModel : BaseViewModel
+    {
+        #region Private Members
+
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// A flag to let us know if we can reveal another card
+        /// </summary>
+        public bool CanReveal => CardsRevealed < 2;
+
+        /// <summary>
+        /// A counter for the number of cards revealed
+        /// </summary>
+        public int CardsRevealed { get; set; } = 0;
+
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// The command for counting the revealed cards
+        /// </summary>
+        public ICommand CardRevealedCommand { get; set; }
+
+        /// <summary>
+        /// The command to reset the cards revealed
+        /// </summary>
+        public ICommand ResetCardsRevealedCommand { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public MemoryViewModel()
+        {
+            CardRevealedCommand = new RelayCommand(() => CardsRevealed++);
+            ResetCardsRevealedCommand = new RelayCommand(() => CardsRevealed = 0);
+        }
+
+        #endregion
+
+    }
+}
