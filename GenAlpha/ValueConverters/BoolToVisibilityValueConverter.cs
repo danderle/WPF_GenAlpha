7bc6106add3930548a6711bce003361de9f15cd3
danderle <danderle86@gmail.com>
2021-06-02 07:25:29 +0200

    Added a bool to visibility converter

---
+++ GenAlpha/ValueConverters/BoolToVisibilityValueConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace GenAlpha
+{
+    /// <summary>
+    /// Converts the a bool to visibility
+    /// </summary>
+    public class BoolToVisibilityValueConverter : BaseValueConverter<BoolToVisibilityValueConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isVisible = (bool)value;
+            return isVisible ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
