563e017c5feb947992bdc7b6ff887ab8371176dc
danderle <danderle86@gmail.com>
2021-06-03 23:31:11 +0200

    Created a SettingsType to Visibility converter

---
+++ GenAlpha/ValueConverters/SettingTypeToVisibilityValueConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace GenAlpha
+{
+    /// <summary>
+    /// Converts a <see cref="SettingTypes"/>to visibility
+    /// </summary>
+    public class SettingTypeToVisibilityValueConverter : BaseValueConverter<SettingTypeToVisibilityValueConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var setting = value.ToString();
+            var converterParameter = (string)parameter;
+            return setting == converterParameter ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
