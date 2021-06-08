8034d9a86ca28b003d73ed39f4ee0bc25fb27b8c
danderle <danderle86@gmail.com>
2021-06-08 22:11:24 +0200

    Added the Settings list and items control with view models. Also added for desing time purposes some design models

---
+++ GenAlpha/UserControls/SettingsListControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace GenAlpha
+{
+    /// <summary>
+    /// Interaction logic for SettingsListControl.xaml
+    /// </summary>
+    public partial class SettingsListControl : UserControl
+    {
+        public SettingsListControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GenAlpha/ViewModels/Controls/SettingsListViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+
+namespace GenAlpha
+{
+    /// <summary>
+    /// The view model for a settings list
+    /// </summary>
+    public class SettingsListViewModel : BaseViewModel
+    {
+        #region Properties
+
+        public ObservableCollection<SettingsListItemViewModel> SettingItems { get; set; } = new ObservableCollection<SettingsListItemViewModel>();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public SettingsListViewModel()
+        {
+
+        }
+
+        #endregion
+
+    }
+}
