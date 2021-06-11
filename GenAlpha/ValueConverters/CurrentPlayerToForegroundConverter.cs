a17fe63ab7ac3e9bd42ec9f2ed2600b8449f009e
danderle <danderle86@gmail.com>
2021-06-11 07:06:57 +0200

    Added the CurrentPlayerToForegroundConverter.cs which changes the current players foreground color

---
+++ GenAlpha/ValueConverters/CurrentPlayerToForegroundConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Media;
+
+namespace GenAlpha
+{
+    /// <summary>
+    /// Converts a <see cref="PlayerTurn"/>to Color
+    /// </summary>
+    public class CurrentPlayerToForegroundConverter : BaseValueConverter<CurrentPlayerToForegroundConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value.ToString() == parameter.ToString())
+            {
+                return new SolidColorBrush((Color)Application.Current.FindResource("PurpleRed"));
+            }
+            return new SolidColorBrush((Color)Application.Current.FindResource("GrayBlue"));
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
