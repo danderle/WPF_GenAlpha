78794b2152470d6757e19026840f5c9b68c61ef5
danderle <danderle86@gmail.com>
2021-06-14 22:14:40 +0200

    Added a static Sound class to play sounds. Added sound files and enum SoundTypes.cs

---
+++ GenAlpha/DataModels/SoundTypes.cs
@@ -0,0 +1,14 @@
+﻿namespace GenAlpha
+{
+    /// <summary>
+    /// The sound types
+    /// </summary>
+    public enum SoundTypes
+    {
+        CardFlip,
+        CardMatch,
+        CardNoMatch,
+        CardSpinOut,
+    }
+
+}
---
+++ GenAlpha/Models/Sound.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace GenAlpha
+{
+    public static class Sound
+    {
+
+        private const string CARD_FLIP_SOUND_FILEPATH = @"Resources/Sounds/CardFlip.wav";
+        private const string CARD_MATCH_SOUND_FILEPATH = @"Resources/Sounds/Pass.mp3";
+        private const string CARD_NOMATCH_SOUND_FILEPATH = @"Resources/Sounds/Fail.wav";
+        private const string CARD_SPINOUT_SOUND_FILEPATH = @"Resources/Sounds/CardSpinOut.mp3";
+
+        private static readonly Dictionary<SoundTypes, string> soundPaths = new();
+        private static MediaPlayer mediaPlayer = new ();
+
+        static Sound()
+        {
+            soundPaths.Add(SoundTypes.CardFlip, CARD_FLIP_SOUND_FILEPATH);
+            soundPaths.Add(SoundTypes.CardMatch, CARD_MATCH_SOUND_FILEPATH);
+            soundPaths.Add(SoundTypes.CardNoMatch, CARD_NOMATCH_SOUND_FILEPATH);
+            soundPaths.Add(SoundTypes.CardSpinOut, CARD_SPINOUT_SOUND_FILEPATH);
+        }
+
+        public static void Play(SoundTypes sound)
+        {
+            mediaPlayer.Open(Path(sound));
+            mediaPlayer.Position = TimeSpan.Zero;
+            mediaPlayer.Play();
+        }
+
+        private static Uri Path(SoundTypes sound)
+        {
+            return new Uri(soundPaths[sound], UriKind.Relative);
+        }
+    }
+}
