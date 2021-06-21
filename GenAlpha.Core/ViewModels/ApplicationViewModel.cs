625722a1eb1b0dfc38a39c964c4a4166a97f09aa
danderle <danderle86@gmail.com>
2021-06-21 06:44:27 +0200

    In the Core project added an inversion of control class, to be able to use dependancy injection. Also added the ApplicationViewModel which is bound in the IoC class

---
+++ GenAlpha.Core/ViewModels/ApplicationViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GenAlpha.Core
+{
+    /// <summary>
+    /// The application state as a view model
+    /// </summary>
+    public class ApplicationViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// The current page of the application
+        /// </summary>
+        public ApplicationPage CurrentPage { get; set; } = ApplicationPage.GameSelection;
+
+    }
+}
