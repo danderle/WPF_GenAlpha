6bcaec6da1bfecf4aa3477957d6ad1ef70f84b84
danderle <danderle86@gmail.com>
2021-06-26 08:09:13 +0200

    Added the Noframe history to remove the navigation bar and clearing the page history. Implemented the pageHost in the MainWindow.xaml

---
+++ GenAlpha/AttachedProperties/NoFrameHistory.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace GenAlpha
+{
+    /// <summary>
+    /// The no frame history attached property for creating a <see cref="Frame"/> that never shows navigation 
+    /// and keeps the navigation history empty
+    /// </summary>
+    public class NoFrameHistory : BaseAttachedProperties<NoFrameHistory, bool>
+    {
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            // Get the frame
+            var frame = (sender as Frame);
+
+            // Hide the navigation bar
+            frame.NavigationUIVisibility = NavigationUIVisibility.Hidden;
+
+            // Clear history on navigate
+            frame.Navigated += (ss, ee) => ((Frame)ss).NavigationService.RemoveBackEntry();
+        }
+    }
+}
