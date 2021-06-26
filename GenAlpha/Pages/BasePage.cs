73c05290e6352df21583b2e336700af27ecef7e3
danderle <danderle86@gmail.com>
2021-06-26 08:08:15 +0200

    Changed the Added a non generic BasePage

--- GenAlpha/Pages/BasePage.cs
+++ GenAlpha/Pages/BasePage.cs
@@ -7,9 +7,118 @@ using System.Windows.Controls;
 namespace GenAlpha
 {
     /// <summary>
+    /// The base page for all pages to gain base functionality
+    /// </summary>
+    public class BasePage : Page
+    {
+         #region Public Properties
+
+        /// <summary>
+        /// The animation the play when the page is first loaded
+        /// </summary>
+        public PageAnimationTypes PageLoadAnimation { get; set; } = PageAnimationTypes.SlideAndFadeInFromRight;
+
+        /// <summary>
+        /// The animation the play when the page is unloaded
+        /// </summary>
+        public PageAnimationTypes PageUnloadAnimation { get; set; } = PageAnimationTypes.SlideAndFadeOutToLeft;
+
+        /// <summary>
+        /// The time any slide animation takes to complete
+        /// </summary>
+        public float SlideSeconds { get; set; } = 0.4f;
+
+        /// <summary>
+        /// A flag to indicate if this page should animate out on load.
+        /// Useful for when we are moving the page to another frame
+        /// </summary>
+        public bool ShouldAnimateOut { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public BasePage()
+        {
+            //Hide at begin of animatioon
+            if (PageLoadAnimation != PageAnimationTypes.None)
+            {
+                Visibility = Visibility.Collapsed;
+            }
+
+            //listen out for the page loading
+            Loaded += BasePage_LoadedAsync;
+        }
+
+        #endregion
+
+        #region Animation Load / Unload
+
+        /// <summary>
+        /// Once the page is loaded, perform any set animation
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void BasePage_LoadedAsync(object sender, RoutedEventArgs e)
+        {
+            // If we are setup to animate out on load
+            if (ShouldAnimateOut)
+            {
+                // Animate out the page
+                await AnimateOutAsync();
+            }
+            // Otherwise...
+            else
+            {
+                // Animate the page in
+                await AnimateInAsync(); 
+            }
+        }
+
+        /// <summary>
+        /// Animates the page in
+        /// </summary>
+        /// <returns></returns>
+        public async Task AnimateInAsync()
+        {
+            if (PageLoadAnimation == PageAnimationTypes.None)
+                return;
+
+            switch (PageLoadAnimation)
+            {
+                case PageAnimationTypes.SlideAndFadeInFromRight:
+                    await this.SlideAndFadeInFromRight(SlideSeconds);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Animates the page out
+        /// </summary>
+        /// <returns></returns>
+        public async Task AnimateOutAsync()
+        {
+            if (PageLoadAnimation == PageAnimationTypes.None)
+                return;
+
+            switch (PageUnloadAnimation)
+            {
+                case PageAnimationTypes.SlideAndFadeOutToLeft:
+                    await this.SlideAndFadeOutToLeft(SlideSeconds);
+                    break;
+            }
+        }
+
+        #endregion
+    }
+
+    /// <summary>
     /// A base page for all pages to gain base functionality
     /// </summary>
-    public class BasePage<VM> : Page
+    public class BasePage<VM> : BasePage
         where VM : BaseViewModel, new()
     {
         #region Private Fields
@@ -24,30 +133,15 @@ namespace GenAlpha
         #region Public Properties
 
         /// <summary>
-        /// The animation to play when the pages is first loaded
-        /// </summary>
-        public PageAnimationTypes PageLoadAnimation { get; set; } = PageAnimationTypes.SlideAndFadeInFromRight; 
-        
-        /// <summary>
-        /// The animation to play when the page is unloaded
-        /// </summary>
-        public PageAnimationTypes PageUnloadAnimation { get; set; } = PageAnimationTypes.SlideAndFadeOutToLeft;
-
-        /// <summary>
-        /// The time to complete the slide animation
-        /// </summary>
-        public float SlidesSeconds { get; set; } = 0.8f;
-
-        /// <summary>
         /// The View model for this page
         /// </summary>
-        public VM ViewModel 
-        { 
+        public VM ViewModel
+        {
             get => _viewModel;
             set
             {
                 //if value changed
-                if(_viewModel != value)
+                if (_viewModel != value)
                 {
                     //Set view model
                     _viewModel = value;
@@ -56,9 +150,9 @@ namespace GenAlpha
                 }
             }
         }
-        
-        #endregion
 
+        #endregion
+        
         #region Constructor
 
         /// <summary>
@@ -70,65 +164,9 @@ namespace GenAlpha
             {
                 return;
             }
-            //Hide at begin of animatioon
-            if(PageLoadAnimation != PageAnimationTypes.None)
-            {
-                Visibility = Visibility.Collapsed;
-            }
-
-            //listen out for the page loading
-            Loaded += BasePage_Loaded;
-
+            
             //Create a default view model
             ViewModel = new VM();
-        }
-
-        #endregion
-
-        #region Animation Load / Unload
-
-        /// <summary>
-        /// Once the page is loaded, perform any set animation
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private async void BasePage_Loaded(object sender, System.Windows.RoutedEventArgs e)
-        {
-            await AnimateInAsync();
-        }
-
-        /// <summary>
-        /// Animates the page in
-        /// </summary>
-        /// <returns></returns>
-        public async Task AnimateInAsync()
-        {
-            if (PageLoadAnimation == PageAnimationTypes.None)
-                return;
-
-            switch(PageLoadAnimation)
-            {
-                case PageAnimationTypes.SlideAndFadeInFromRight:
-                    await this.SlideAndFadeInFromRight(SlidesSeconds);
-                    break;
-            }
-        }
-        
-        /// <summary>
-        /// Animates the page out
-        /// </summary>
-        /// <returns></returns>
-        public async Task AnimateOutAsync()
-        {
-            if (PageLoadAnimation == PageAnimationTypes.None)
-                return;
-
-            switch(PageUnloadAnimation)
-            {
-                case PageAnimationTypes.SlideAndFadeOutToLeft:
-                    await this.SlideAndFadeOutToLeft(SlidesSeconds);
-                    break;
-            }
         }
 
         #endregion
