83f85e723ed0f3c3367aa5c3a7638ff5608bc420
danderle <danderle86@gmail.com>
2021-07-13 15:51:49 +0200

    Implemented all the custom properties for handling the falling texts and bullets

--- GenAlpha/AttachedProperties/CanvasAttachedProperties.cs
+++ GenAlpha/AttachedProperties/CanvasAttachedProperties.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using GenAlpha.Core;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using static GenAlpha.Core.KeyboardShooterViewModel;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace GenAlpha
 {
@@ -11,14 +13,16 @@ namespace GenAlpha
     /// </summary>
     public class SpawnItem : BaseAttachedProperties<SpawnItem, bool>
     {
+        private object fallingTexts = new();
+
         public override void OnValueUpdated(DependencyObject sender, object value)
         {
             var canvas = sender as Canvas;
             if (canvas == null)
                 return;
 
-            var items = TextObjects.Get(canvas);
-            if (items == null || items.Count == 0)
+            var texts = TextObjects.Get(canvas);
+            if (texts == null || texts.Count == 0)
                 return;
 
             var childrenCount = canvas.Children.Count;
@@ -27,25 +31,74 @@ namespace GenAlpha
                 canvas.Children.RemoveRange(1, childrenCount - 1);
             }
 
-            foreach (var item in items)
+            lock(fallingTexts)
             {
-                var textBlock = new TextBlock();
-                textBlock.Text = item.Text;
-                Canvas.SetLeft(textBlock, item.Xposition);
-                Canvas.SetTop(textBlock, item.Yposition);
-                canvas.Children.Add(textBlock);
+                TextObjects.AddTextsToCanvas(texts, canvas);
             }
         }
     }
 
+    #region TextObjects
+
+    /// <summary>
+    /// Gets the FallingTexts items
+    /// </summary>
     public class TextObjects : BaseAttachedProperties<TextObjects, List<FallingText>>
     {
         public static List<FallingText> Get(DependencyObject d)
         {
             return GetValue(d);
         }
+
+        public static void AddTextsToCanvas(List<FallingText> texts, Canvas canvas)
+        {
+            foreach (var text in texts)
+            {
+                var textBlock = new TextBlock();
+                textBlock.Text = text.DisplayedText;
+                Canvas.SetLeft(textBlock, text.Xposition);
+                Canvas.SetTop(textBlock, text.Yposition);
+                canvas.Children.Add(textBlock);
+            }
+        }
     }
-    
+
+    #endregion
+
+    #region BulletObjects
+
+    /// <summary>
+    /// Gets the bullet objects
+    /// </summary>
+    public class BulletObjects : BaseAttachedProperties<TextObjects, List<Bullet>>
+    {
+        public static List<Bullet> Get(DependencyObject d)
+        {
+            return GetValue(d);
+        }
+
+        public static void AddBulletsToCanvas(List<Bullet> bullets, Canvas canvas)
+        {
+            foreach (var bullet in bullets)
+            {
+                var ellipse = new Ellipse();
+                ellipse.Width = 10;
+                ellipse.Height = 10;
+                ellipse.Fill = new SolidColorBrush((Color)Application.Current.FindResource("Gray"));
+                Canvas.SetLeft(ellipse, bullet.Xposition);
+                Canvas.SetTop(ellipse, bullet.Yposition);
+                canvas.Children.Add(ellipse);
+            }
+        }
+    }
+
+    #endregion
+
+    #region ItemsMoved
+
+    /// <summary>
+    /// Lets us know when to update falling texts and bullets
+    /// </summary>
     public class ItemsMoved : BaseAttachedProperties<ItemsMoved, bool>
     {
         public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
@@ -54,8 +107,8 @@ namespace GenAlpha
             if (canvas == null)
                 return;
 
-            var items = TextObjects.Get(canvas);
-            if (items == null || items.Count == 0)
+            var texts = TextObjects.Get(canvas);
+            if (texts == null || texts.Count == 0)
                 return;
 
             var childrenCount = canvas.Children.Count;
@@ -64,17 +117,23 @@ namespace GenAlpha
                 canvas.Children.RemoveRange(1, childrenCount - 1);
             }
 
-            foreach (var item in items)
-            {
-                var textBlock = new TextBlock();
-                textBlock.Text = item.Text;
-                Canvas.SetLeft(textBlock, item.Xposition);
-                Canvas.SetTop(textBlock, item.Yposition);
-                canvas.Children.Add(textBlock);
-            }
+            TextObjects.AddTextsToCanvas(texts, canvas);
+
+            var bullets = BulletObjects.Get(canvas);
+            if (bullets == null || bullets.Count == 0)
+                return;
+
+            BulletObjects.AddBulletsToCanvas(bullets, canvas);
         }
     }
 
+    #endregion
+
+    #region KeyboardInput
+
+    /// <summary>
+    /// Executes the command with the key input as a parameter
+    /// </summary>
     public class KeyboardInput : BaseAttachedProperties<KeyboardInput, ICommand>
     {
         private ICommand command;
@@ -96,4 +155,5 @@ namespace GenAlpha
         }
     }
 
+    #endregion
 }
