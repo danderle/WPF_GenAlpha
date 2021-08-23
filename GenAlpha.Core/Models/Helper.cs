1dcc1e4fd94c7bddc5935ed91cd0bd572898e55b
danderle <danderle86@gmail.com>
2021-08-23 07:03:38 +0200

    Added a Helper.cs with a shuffle function

---
+++ GenAlpha.Core/Models/Helper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenAlpha.Core
+{
+    public static class Helper
+    {
+
+        public static List<T> Shuffle<T>(this List<T> list)
+        {
+            Random random = new Random();
+
+            for (int i = list.Count - 1; i > 1; i--)
+            {
+                int rnd = random.Next(i + 1);
+                var value = list[rnd];
+
+                list[rnd] = list[i];
+                list[i] = value;
+            }
+            return list;
+        }
+    }
+}
