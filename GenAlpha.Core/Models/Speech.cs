493200f520db6d2b82231e0e4f9cc11863a330e0
danderle <danderle86@gmail.com>
2021-10-08 07:12:02 +0200

    Added a speech class to sound out letters and words

---
+++ GenAlpha.Core/Models/Speech.cs
@@ -0,0 +1,53 @@
+﻿using System.Globalization;
+using System.Speech.Synthesis;
+
+namespace GenAlpha.Core
+{
+    /// <summary>
+    /// Static class to read out words in the specified language
+    /// </summary>
+    public static class Speech
+    {
+        #region Fields
+
+        /// <summary>
+        /// The speech Synthesizer to use
+        /// </summary>
+        private static SpeechSynthesizer synth;
+
+        #endregion
+
+        #region static constructor
+
+        /// <summary>
+        /// Static constructor
+        /// </summary>
+        static Speech()
+        {
+            // init speech snythesizer
+            synth = new SpeechSynthesizer();
+            // Configure the audio output.   
+            synth.SetOutputToDefaultAudioDevice();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Reads out the given text in the given language
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="culture"></param>
+        public static void Speak(string text, string culture)
+        {
+            // Speak word
+            var prompt = new PromptBuilder(new CultureInfo(culture));
+            prompt.AppendText(text);
+            synth.SpeakAsync(prompt);
+
+        }
+
+        #endregion
+    }
+}
