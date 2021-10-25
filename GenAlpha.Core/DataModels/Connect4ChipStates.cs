b735be7014d70b848b52adb77c52a0aa64533870
danderle <danderle86@gmail.com>
2021-10-25 19:25:52 +0200

    Added a Connect4ChipStates enum

---
+++ GenAlpha.Core/DataModels/Connect4ChipStates.cs
@@ -0,0 +1,14 @@
+﻿namespace GenAlpha.Core
+{
+    /// <summary>
+    /// The connect 4 chip states
+    /// </summary>
+    public enum Connect4ChipStates
+    {
+        StartUnselected,
+        Player1,
+        Player2,
+        GameOverUnselected,
+    }
+
+}
