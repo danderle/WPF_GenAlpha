acc2626f99760cf5dc2d98e96e63907a1d6625c3
danderle <danderle86@gmail.com>
2021-10-25 19:28:42 +0200

    Changed the RGBhexToColorConverter.cs to ChipStateToColorConverter.cs

---
+++ GenAlpha/ValueConverters/ChipStateToColorConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Media;
+using GenAlpha.Core;
+
+namespace GenAlpha
+{
+    /// <summary>
+    /// Converts a <see cref="Connect4ChipStates"/>to Color
+    /// </summary>
+    public class ChipStateToColorConverter : BaseValueConverter<ChipStateToColorConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Connect4ChipStates chipState = (Connect4ChipStates)value;
+            switch(chipState)
+            {
+                case Connect4ChipStates.StartUnselected:
+                    return new SolidColorBrush((Color)Application.Current.FindResource("LightGray"));
+                case Connect4ChipStates.Player1:
+                    return new SolidColorBrush((Color)Application.Current.FindResource("Player1Red"));
+                case Connect4ChipStates.Player2:
+                    return new SolidColorBrush((Color)Application.Current.FindResource("Player2Yellow"));
+                case Connect4ChipStates.GameOverUnselected:
+                    return new SolidColorBrush((Color)Application.Current.FindResource("VeryDarkBlue"));
+                default:
+                    Debugger.Break();
+                    return new SolidColorBrush((Color)Application.Current.FindResource("LightGray"));
+            }
+
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
