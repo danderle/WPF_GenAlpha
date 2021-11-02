51ec2554937bae8f10b324873fdc3dd1892813b5
danderle <danderle86@gmail.com>
2021-11-02 06:39:05 +0100

    Added the MineseeperSqaureState enum

---
+++ GenAlpha.Core/DataModels/MinesweeperSquareState.cs
@@ -0,0 +1,22 @@
+﻿namespace GenAlpha.Core
+{
+    /// <summary>
+    /// The state of squares in minesweeper
+    /// </summary>
+    public enum MinesweeperSquareState
+    {
+        Zero,
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Unopened,
+        Flag,
+        Bomb
+    }
+
+}
