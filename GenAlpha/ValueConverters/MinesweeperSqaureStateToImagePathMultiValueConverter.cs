08fb2af17439c7aa66bca45bd8d2f1e080b8ab34
danderle <danderle86@gmail.com>
2021-11-06 09:39:38 +0100

    Implemented the MinesweeperSqaureStateToImagePathMultiValueConverter

---
+++ GenAlpha/ValueConverters/MinesweeperSqaureStateToImagePathMultiValueConverter.cs
@@ -0,0 +1,67 @@
+﻿using GenAlpha.Core;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows;
+
+namespace GenAlpha
+{
+    /// <summary>
+    /// Converts a <see cref="MinesweeperSqaureState"/>to an image path
+    /// </summary>
+    public class MinesweeperSqaureStateToImagePathMultiValueConverter : BaseMultiValueConverter<MinesweeperSqaureStateToImagePathMultiValueConverter>
+    {
+        public override object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)
+        {
+            MinesweeperSquareState state = (MinesweeperSquareState)value[1];
+            if (value[0] is bool && (bool)value[0])
+            {
+                switch (state)
+                {
+                    case MinesweeperSquareState.Zero:
+                        return Application.Current.FindResource("0");
+                    case MinesweeperSquareState.One:
+                        return Application.Current.FindResource("1");
+                    case MinesweeperSquareState.Two:
+                        return Application.Current.FindResource("2");
+                    case MinesweeperSquareState.Three:
+                        return Application.Current.FindResource("3");
+                    case MinesweeperSquareState.Four:
+                        return Application.Current.FindResource("4");
+                    case MinesweeperSquareState.Five:
+                        return Application.Current.FindResource("5");
+                    case MinesweeperSquareState.Six:
+                        return Application.Current.FindResource("6");
+                    case MinesweeperSquareState.Seven:
+                        return Application.Current.FindResource("7");
+                    case MinesweeperSquareState.Eight:
+                        return Application.Current.FindResource("8");
+                    case MinesweeperSquareState.Flag:
+                        return Application.Current.FindResource("Flag");
+                    case MinesweeperSquareState.Bomb:
+                        return Application.Current.FindResource("Bomb");
+                    case MinesweeperSquareState.Unopened:
+                        return Application.Current.FindResource("Unopened");
+                    default:
+                        Debugger.Break();
+                        return Application.Current.FindResource("Unopened");
+                }
+            }
+            else
+            {
+                switch (state)
+                {
+                    case MinesweeperSquareState.Flag:
+                        return Application.Current.FindResource("Flag");
+                    default:
+                        return Application.Current.FindResource("Unopened");
+                }
+            }
+        }
+
+        public override object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
