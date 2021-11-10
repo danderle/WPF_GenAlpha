4215ba943d3b90e4848ab7c06156963f23393e23
danderle <danderle86@gmail.com>
2021-11-10 23:45:44 +0100

    Added a MinesweeperSideMenuViewmodel and Control

---
+++ GenAlpha.Core/ViewModels/Controls/MinesweeperSideMenuViewModel.cs
@@ -0,0 +1,31 @@
+﻿namespace GenAlpha.Core
+{
+    /// <summary>
+    /// The view model for the side menu control
+    /// </summary>
+    public class MinesweeperSideMenuViewModel : BaseViewModel
+    {
+        #region Properties
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public MinesweeperSideMenuViewModel()
+        {
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+    }
+}
