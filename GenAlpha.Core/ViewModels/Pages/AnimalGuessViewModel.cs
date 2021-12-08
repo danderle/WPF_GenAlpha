23840f8c538eb8cbcb2fdb96c81c4bd1f3d519ba
danderle <danderle86@gmail.com>
2021-12-08 16:11:36 +0100

    Implemented the BaseTopBarViewModel and the MouseMoveCommand

--- GenAlpha.Core/ViewModels/Pages/AnimalGuessViewModel.cs
+++ GenAlpha.Core/ViewModels/Pages/AnimalGuessViewModel.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Timers;
+﻿using System;
+using System.Windows;
 using System.Windows.Input;
 
 namespace GenAlpha.Core
@@ -10,6 +9,29 @@ namespace GenAlpha.Core
     /// </summary>
     public class AnimalGuessViewModel : BaseViewModel
     {
+        #region Public Properties
+
+        /// <summary>
+        /// The current mouse position
+        /// </summary>
+        public Point MousePosition { get; set; }
+
+        /// <summary>
+        /// The top bar for this view model
+        /// </summary>
+        public BaseTopBarViewModel TopBar { get; set; } = new BaseTopBarViewModel();
+
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// The mouse move command
+        /// </summary>
+        public ICommand MouseMoveCommand { get; set; }
+
+        #endregion
+
         #region Constructor
 
         /// <summary>
@@ -17,6 +39,22 @@ namespace GenAlpha.Core
         /// </summary>
         public AnimalGuessViewModel()
         {
+            MouseMoveCommand = new RelayParameterizedCommand(MouseMoved);
+        }
+
+        #endregion
+
+        #region Command Methods
+
+        /// <summary>
+        /// Seths the current mouse position
+        /// </summary>
+        /// <param name="obj"></param>
+        private void MouseMoved(object obj)
+        {
+            string position = obj.ToString();
+            var coo = position.Split(";");
+            MousePosition = new Point(Convert.ToDouble(coo[0]), Convert.ToDouble(coo[1]));
         }
 
         #endregion
