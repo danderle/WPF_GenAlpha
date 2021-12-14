dde35fbf4d4ec38f8766140bd93f405a141dee14
danderle <danderle86@gmail.com>
2021-12-14 06:24:19 +0100

    Split the mouse move, enter and leave into the separate attached properties

--- GenAlpha/AttachedProperties/BorderAttachedProperties.cs
+++ GenAlpha/AttachedProperties/BorderAttachedProperties.cs
@@ -3,20 +3,78 @@ using System.Windows.Input;
 
 namespace GenAlpha
 {
-    #region MouseMoved
+    #region Mouse
 
     /// <summary>
-    /// Executes the command with the mouse position as a parameter
+    /// Executes the command on the mouse enter event
     /// </summary>
-    public class MouseMoved : BaseAttachedProperties<MouseMoved, ICommand>
+    public class MouseEnter : BaseAttachedProperties<MouseEnter, ICommand>
     {
+        #region Command
+
         /// <summary>
         /// The command to execute
         /// </summary>
         private ICommand command;
 
+        #endregion
+
+        #region OnValueChanged
+
         /// <summary>
-        /// Sets the command and registers the MouseEnter and MouseLeave events
+        /// Sets the command and registers the MouseEnter event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            // Check if sender is a framework element
+            if (!(sender is FrameworkElement element))
+            {
+                return;
+            }
+
+            // save the command
+            command = (ICommand)e.NewValue;
+
+            // Register mouse enter event
+            element.MouseEnter += Element_MouseEnter;
+        }
+
+        #endregion
+
+        #region Mouse enter event
+
+        /// <summary>
+        /// The mouse enter event executes the command
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Element_MouseEnter(object sender, MouseEventArgs e)
+        {
+            command.Execute(null);
+        } 
+        #endregion
+    }
+
+    /// <summary>
+    /// Executes the command on the mouse leave event
+    /// </summary>
+    public class MouseLeave : BaseAttachedProperties<MouseLeave, ICommand>
+    {
+        #region Command
+
+        /// <summary>
+        /// The command to execute
+        /// </summary>
+        private ICommand command;
+
+        #endregion
+
+        #region OnValueChanged
+
+        /// <summary>
+        /// Sets the command and registers the MouseLeave event
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -32,42 +90,56 @@ namespace GenAlpha
             command = (ICommand)e.NewValue;
 
             // Register mouse enter and leave events
-            element.MouseEnter += Element_MouseEnter;
             element.MouseLeave += Element_MouseLeave;
         }
 
+        #endregion
+
+        #region Mouse enter event
+
         /// <summary>
-        /// The event method on mouse leave, unhooks the mouse move event
+        /// The mouse leave event executes the command
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void Element_MouseLeave(object sender, MouseEventArgs e)
         {
-            if (!(sender is FrameworkElement element))
-            {
-                return;
-            }
-
-            element.MouseMove -= Element_MouseMove;
+            command.Execute(null);
         }
+        #endregion
+    }
+
+    /// <summary>
+    /// Executes the command with the mouse position as a parameter
+    /// </summary>
+    public class MouseMoved : BaseAttachedProperties<MouseMoved, ICommand>
+    {
+        /// <summary>
+        /// The command to execute
+        /// </summary>
+        private ICommand command;
 
         /// <summary>
-        /// The mouse enter event method hooks into the mouse move event
+        /// Sets the command and registers the MouseMove event
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void Element_MouseEnter(object sender, MouseEventArgs e)
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
         {
+            // Check if sender is a framework element
             if (!(sender is FrameworkElement element))
             {
                 return;
             }
+
+            // save the command
+            command = (ICommand)e.NewValue;
 
             element.MouseMove += Element_MouseMove;
         }
 
         /// <summary>
-        /// The mouse move event method gets the current mouse position and executes the commmand
+        /// The mouse move event method executes the commmand with the mouse position
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -78,10 +150,11 @@ namespace GenAlpha
                 return;
             }
 
-            Point mousePosition = e.GetPosition(sender as FrameworkElement);
+            Point mousePosition = e.GetPosition(element);
             command.Execute(mousePosition);
         }
     }
 
     #endregion
+
 }
