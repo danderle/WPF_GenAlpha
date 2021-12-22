e7d2e1a8f36eed3d3a069366261e137431603c7e
danderle <danderle86@gmail.com>
2021-12-22 07:21:38 +0100

    The AnimalChoiceViewModel is used for the possible guess choices

---
+++ GenAlpha.Core/ViewModels/Controls/AnimalChoiceViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows.Input;
+
+namespace GenAlpha.Core
+{
+    /// <summary>
+    /// A class for describing the possible animal choices in the animal guess game
+    /// </summary>
+    public class AnimalChoiceViewModel : BaseViewModel
+    {
+        #region Properties
+
+        /// <summary>
+        /// A flag to let us know if this is the hidden animal
+        /// </summary>
+        public bool IsHiddenAnimal { get; set; }
+
+        /// <summary>
+        /// The name possible choice name
+        /// </summary>
+        public string ChoiceName { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// The command to check if the animal is correct
+        /// </summary>
+        public ICommand AnimalFoundCommand { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// The default constructor
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="command"></param>
+        public AnimalChoiceViewModel(string name, ICommand command)
+        {
+            ChoiceName = name;
+            AnimalFoundCommand = command;
+        } 
+
+        #endregion
+    }
+}
