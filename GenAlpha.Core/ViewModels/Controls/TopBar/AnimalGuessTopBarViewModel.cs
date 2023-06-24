a40b26c6a93e461cd5e6e7e49a5559d1fe28e5c7
danderle <danderle86@gmail.com>
2023-06-24 08:44:25 +0200

    Merge

---
+++ GenAlpha.Core/ViewModels/Controls/TopBar/AnimalGuessTopBarViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace GenAlpha.Core
+{
+    /// <summary>
+    /// The view model for the <see cref="TopBarControl"/>
+    /// </summary>
+    public class AnimalGuessTopBarViewModel : BaseTopBarViewModel
+    {
+        #region Fields
+
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Actions
+
+        /// <summary>
+        /// Action to toggle the side menu
+        /// </summary>
+        public Action ToggelSideMenu;
+
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// The command to show/hidew the side menu
+        /// </summary>
+        public ICommand ToggleSideMenuCommand { get; set; }
+
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public AnimalGuessTopBarViewModel()
+        {
+            InitializeCommands();
+        }
+
+        #endregion
+
+        #region Command Methods
+
+        /// <summary>
+        /// The command method to show / hide the side menu
+        /// </summary>
+        private void ToggleSideMenu()
+        {
+            ToggelSideMenu();
+        }
+
+        #endregion
+
+        #region Public Methods
+        
+        
+
+        #endregion
+
+        #region Private Helpers
+
+        /// <summary>
+        /// Initializes the commmands
+        /// </summary>
+        private void InitializeCommands()
+        {
+            ToggleSideMenuCommand = new RelayCommand(ToggleSideMenu);
+        }
+
+        #endregion
+    }
+}
